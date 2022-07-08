a4d4fe1da19f5a3642f19ed17566bed6b0212ad1
shgtkshruch <shgtk.shruch@gmail.com>
2022-07-08 12:23:13 +0900

    Add custom hooks to fetch data from GitHub with GraphQL

---
+++ src/useGitHubAPI.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+import { GraphQLClient } from 'graphql-request';
+
+const endpoint = 'https://api.github.com/graphql';
+
+export function useGitHubAPI(query) {
+  const [data, setData] = useState(null);
+
+  const graphQLClient = new GraphQLClient(endpoint, {
+    headers: {
+      Authorization: `Bearer ${process.env.REACT_APP_GITHUB_ACCESS_TOKEN}`,
+    },
+  })
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await graphQLClient.request(query);
+      setData(response);
+    }
+    console.log('fetch data');
+    fetchData();
+  }, []);
+
+  return data;
+}
+
